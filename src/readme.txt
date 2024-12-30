e15861c53602c76ac3515c42ef356c730b1afa1a
RAFZIN <rafzin@Rafzins-MacBook-Air.local>
2024-12-30 16:23:57 +0530

    images for bloodtesting

---
+++ src/readme.txt
@@ -0,0 +1,75 @@
+
+
+5/12/2024 - tinjo
+Output css issue fixed light and weight 
+
+Terminal only working this 
+css automatic created 
+
+terminal command
+Start the Tailwind CLI build process
+Run the CLI tool to scan your template files for classes and build your CSS.
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+
+width and height
+w-56 =
+1=4
+
+
+custom design system.
+Tailwind config.js 
+Custom color
+bg-[#000]
+text-[fff]
+
+https://tailwindcss.com/docs/theme
+
+Basic
+react:
+
+code learn dev
+https://www.udemy.com/course/the-complete-web-development-bootcamp/
+
+
+
+Domain purchase 
+
+Misspell
+.com
+Namecheap : without Gst
+Long duration puchased date.
+
+Hosting : future 
+Aws - digital ocea 
+
+Payment gatway
+Gumroad
+phone pe (service charge less)
+Lemon squeezy 
+
+Selling 
+Desig system, color
+brand quid Selling
+icon Selling
+
+site 
+gumroad add
+sell icon in nounicon 
+etsy
+canva
+
+kit Selling
+https://www.hex.inc
+https://www.madebycircular.com.au
+https://www.faizur.com/figma-thumbnail-kit
+
+Flower gorup site sellers dubai
+https://flowwow.ae/d
+https://www.flowers.ae/
+https://ae.igp.com/
+https://www.interflora.in/
+https://www.theflowershop.ae/?srsltid=AfmBOopWRMeWsSwq794Ghepog0qmYRBfXO4cHAxsb7ufZ7tjfVm0erhs
+
+
+flower selling dxb all dealers
+https://vasees.ae
---
+++ src/script.js
@@ -0,0 +1,380 @@
+// const svgFiles = [
+//   "Add file.svg",
+//   "divide.svg",
+//   "Add image.svg",
+//   "download.svg",
+//   "Align center.svg",
+//   "eye-slash.svg",
+//   "Align left.svg",
+//   "eye.svg",
+//   "Align right.svg",
+//   "file-lock.svg",
+//   "Bold.svg",
+//   "file-upload.svg",
+//   "Book-open.svg",
+//   "file.svg",
+//   "Book.svg",
+//   "filed-download.svg",
+//   "Certificate.svg",
+//   "files.svg",
+//   "Chart.svg",
+//   "flag.svg",
+//   "Document.svg",
+//   "flash.svg",
+//   "Funnel.svg",
+//   "folder-add.svg",
+//   "Hamburge.svg",
+//   "folder.svg",
+//   "Heart.svg",
+//   "globe.svg",
+//   "I progress.svg",
+//   "headphone.svg",
+//   "Info.svg",
+//   "home.svg",
+//   "Minus.svg",
+//   "image.svg",
+//   "Monitor.svg",
+//   "language.svg",
+//   "More.svg",
+//   "like.svg",
+//   "Plus.svg",
+//   "link.svg",
+//   "Search-in.svg",
+//   "loader.svg",
+//   "Search-out.svg",
+//   "lock.svg",
+//   "Search.svg",
+//   "mail.svg",
+//   "Speed service.svg",
+//   "map-pin.svg",
+//   "Table.svg",
+//   "maximis.svg",
+//   "Tag.svg",
+//   "minimise.svg",
+//   "Wallet-1.svg",
+//   "money.svg",
+//   "Wallet.svg",
+//   "package.svg",
+//   "ai.svg",
+//   "paper clip.svg",
+//   "arrow-down-circle.svg",
+//   "pause.svg",
+//   "arrow-left-circle.svg",
+//   "pen.svg",
+//   "arrow-right-circle.svg",
+//   "percent-badge.svg",
+//   "arrow-up-circle.svg",
+//   "percent.svg",
+//   "ban.svg",
+//   "phone-call.svg",
+//   "bell.svg",
+//   "play.svg",
+//   "bookmark.svg",
+//   "power.svg",
+//   "briefcase.svg",
+//   "printer.svg",
+//   "bulb.svg",
+//   "question-mark-circle.svg",
+//   "calendar-add.svg",
+//   "recycled.svg",
+//   "calendar-configure.svg",
+//   "rewrite.svg",
+//   "calendar-mark.svg",
+//   "scan.svg",
+//   "calendar-schedule.svg",
+//   "send.svg",
+//   "calendar.svg",
+//   "shopping-bag.svg",
+//   "camera.svg",
+//   "shopping-cart.svg",
+//   "car.svg",
+//   "sidebar.svg",
+//   "category.svg",
+//   "smile.svg",
+//   "chat.svg",
+//   "star.svg",
+//   "check-badge.svg",
+//   "store.svg",
+//   "check-circle.svg",
+//   "summarise.svg",
+//   "chevron-down.svg",
+//   "sun.svg",
+//   "chevron-left.svg",
+//   "swap.svg",
+//   "chevron-right.svg",
+//   "text enable.svg",
+//   "chevron-up.svg",
+//   "text-disabled.svg",
+//   "circle-down-chevron.svg",
+//   "time schedule.svg",
+//   "circle-left-chevron-1.svg",
+//   "toggle-left.svg",
+//   "circle-left-chevron.svg",
+//   "toggle-right.svg",
+//   "circle-up-chevron.svg",
+//   "trash.svg",
+//   "clipboard.svg",
+//   "trophy.svg",
+//   "clock.svg",
+//   "type.svg",
+//   "close.svg",
+//   "unlock.svg",
+//   "coffee.svg",
+//   "upload.svg",
+//   "cog.svg",
+//   "user-1.svg",
+//   "copy.svg",
+//   "user.svg",
+//   "credit-card.svg",
+//   "users.svg",
+//   "crop-1.svg",
+//   "video.svg",
+//   "crop.svg",
+//   "volume off.svg",
+//   "cub.svg",
+//   "volume up.svg",
+//   "currency-dollar.svg",
+//   "volumn down.svg",
+//   "currency-euro.svg",
+//   "watch.svg",
+//   "currency-rupee.svg",
+//   "wifi.svg",
+//   "database.svg",
+//   "window.svg",
+//   "dislike.svg",
+// ];
+
+// console.log("hi");
+const svgIcons = [
+  { name: "Add file.svg", class: "icon-add-file" },
+  { name: "divide.svg", class: "icon-divide" },
+  { name: "Add image.svg", class: "icon-add-image" },
+  { name: "download.svg", class: "icon-download" },
+  { name: "Align center.svg", class: "icon-align-center" },
+  { name: "eye-slash.svg", class: "icon-eye-slash" },
+  { name: "Align left.svg", class: "icon-align-left" },
+  { name: "eye.svg", class: "icon-eye" },
+  { name: "Align right.svg", class: "icon-align-right" },
+  { name: "file-lock.svg", class: "icon-file-lock" },
+  { name: "Bold.svg", class: "icon-bold" },
+  { name: "file-upload.svg", class: "icon-file-upload" },
+  { name: "Book-open.svg", class: "icon-book-open" },
+  { name: "file.svg", class: "icon-file" },
+  { name: "Book.svg", class: "icon-book" },
+  { name: "filed-download.svg", class: "icon-filed-download" },
+  { name: "Certificate.svg", class: "icon-certificate" },
+  { name: "files.svg", class: "icon-files" },
+  { name: "Chart.svg", class: "icon-chart" },
+  { name: "flag.svg", class: "icon-flag" },
+  { name: "Document.svg", class: "icon-document" },
+  { name: "flash.svg", class: "icon-flash" },
+  { name: "Funnel.svg", class: "icon-funnel" },
+  { name: "folder-add.svg", class: "icon-folder-add" },
+  { name: "Hamburge.svg", class: "icon-hamburge" },
+  { name: "folder.svg", class: "icon-folder" },
+  { name: "Heart.svg", class: "icon-heart" },
+  { name: "globe.svg", class: "icon-globe" },
+  { name: "I progress.svg", class: "icon-i-progress" },
+  { name: "headphone.svg", class: "icon-headphone" },
+  { name: "Info.svg", class: "icon-info" },
+  { name: "home.svg", class: "icon-home" },
+  { name: "Minus.svg", class: "icon-minus" },
+  { name: "image.svg", class: "icon-image" },
+  { name: "Monitor.svg", class: "icon-monitor" },
+  { name: "language.svg", class: "icon-language" },
+  { name: "More.svg", class: "icon-more" },
+  { name: "like.svg", class: "icon-like" },
+  { name: "Plus.svg", class: "icon-plus" },
+  { name: "link.svg", class: "icon-link" },
+  { name: "Search-in.svg", class: "icon-search-in" },
+  { name: "loader.svg", class: "icon-loader" },
+  { name: "Search-out.svg", class: "icon-search-out" },
+  { name: "lock.svg", class: "icon-lock" },
+  { name: "Search.svg", class: "icon-search" },
+  { name: "mail.svg", class: "icon-mail" },
+  { name: "Speed service.svg", class: "icon-speed-service" },
+  { name: "map-pin.svg", class: "icon-map-pin" },
+  { name: "Table.svg", class: "icon-table" },
+  { name: "maximis.svg", class: "icon-maximis" },
+  { name: "Tag.svg", class: "icon-tag" },
+  { name: "minimise.svg", class: "icon-minimise" },
+  { name: "Wallet-1.svg", class: "icon-wallet-1" },
+  { name: "money.svg", class: "icon-money" },
+  { name: "Wallet.svg", class: "icon-wallet" },
+  { name: "package.svg", class: "icon-package" },
+  { name: "ai.svg", class: "icon-ai" },
+  { name: "paper clip.svg", class: "icon-paper-clip" },
+  { name: "arrow-down-circle.svg", class: "icon-arrow-down-circle" },
+  { name: "pause.svg", class: "icon-pause" },
+  { name: "arrow-left-circle.svg", class: "icon-arrow-left-circle" },
+  { name: "pen.svg", class: "icon-pen" },
+  { name: "arrow-right-circle.svg", class: "icon-arrow-right-circle" },
+  { name: "percent-badge.svg", class: "icon-percent-badge" },
+  { name: "arrow-up-circle.svg", class: "icon-arrow-up-circle" },
+  { name: "percent.svg", class: "icon-percent" },
+  { name: "ban.svg", class: "icon-ban" },
+  { name: "phone-call.svg", class: "icon-phone-call" },
+  { name: "bell.svg", class: "icon-bell" },
+  { name: "play.svg", class: "icon-play" },
+  { name: "bookmark.svg", class: "icon-bookmark" },
+  { name: "power.svg", class: "icon-power" },
+  { name: "briefcase.svg", class: "icon-briefcase" },
+  { name: "printer.svg", class: "icon-printer" },
+  { name: "bulb.svg", class: "icon-bulb" },
+  { name: "question-mark-circle.svg", class: "icon-question-mark-circle" },
+  { name: "calendar-add.svg", class: "icon-calendar-add" },
+  { name: "recycled.svg", class: "icon-recycled" },
+  { name: "calendar-configure.svg", class: "icon-calendar-configure" },
+  { name: "rewrite.svg", class: "icon-rewrite" },
+  { name: "calendar-mark.svg", class: "icon-calendar-mark" },
+  { name: "scan.svg", class: "icon-scan" },
+  { name: "calendar-schedule.svg", class: "icon-calendar-schedule" },
+  { name: "send.svg", class: "icon-send" },
+  { name: "calendar.svg", class: "icon-calendar" },
+  { name: "shopping-bag.svg", class: "icon-shopping-bag" },
+  { name: "camera.svg", class: "icon-camera" },
+  { name: "shopping-cart.svg", class: "icon-shopping-cart" },
+  { name: "car.svg", class: "icon-car" },
+  { name: "sidebar.svg", class: "icon-sidebar" },
+  { name: "category.svg", class: "icon-category" },
+  { name: "smile.svg", class: "icon-smile" },
+  { name: "chat.svg", class: "icon-chat" },
+  { name: "star.svg", class: "icon-star" },
+  { name: "check-badge.svg", class: "icon-check-badge" },
+  { name: "store.svg", class: "icon-store" },
+  { name: "check-circle.svg", class: "icon-check-circle" },
+  { name: "summarise.svg", class: "icon-summarise" },
+  { name: "chevron-down.svg", class: "icon-chevron-down" },
+  { name: "sun.svg", class: "icon-sun" },
+  { name: "chevron-left.svg", class: "icon-chevron-left" },
+  { name: "swap.svg", class: "icon-swap" },
+  { name: "chevron-right.svg", class: "icon-chevron-right" },
+  { name: "text enable.svg", class: "icon-text-enable" },
+  { name: "chevron-up.svg", class: "icon-chevron-up" },
+  { name: "text-disabled.svg", class: "icon-text-disabled" },
+  { name: "circle-down-chevron.svg", class: "icon-circle-down-chevron" },
+  { name: "time schedule.svg", class: "icon-time-schedule" },
+  { name: "circle-left-chevron-1.svg", class: "icon-circle-left-chevron-1" },
+  { name: "toggle-left.svg", class: "icon-toggle-left" },
+  { name: "circle-left-chevron.svg", class: "icon-circle-left-chevron" },
+  { name: "toggle-right.svg", class: "icon-toggle-right" },
+  { name: "circle-up-chevron.svg", class: "icon-circle-up-chevron" },
+  { name: "trash.svg", class: "icon-trash" },
+  { name: "clipboard.svg", class: "icon-clipboard" },
+  { name: "trophy.svg", class: "icon-trophy" },
+  { name: "clock.svg", class: "icon-clock" },
+  { name: "type.svg", class: "icon-type" },
+  { name: "close.svg", class: "icon-close" },
+  { name: "unlock.svg", class: "icon-unlock" },
+  { name: "coffee.svg", class: "icon-coffee" },
+  { name: "upload.svg", class: "icon-upload" },
+  { name: "cog.svg", class: "icon-cog" },
+  { name: "user-1.svg", class: "icon-user-1" },
+  { name: "copy.svg", class: "icon-copy" },
+  { name: "user.svg", class: "icon-user" },
+  { name: "credit-card.svg", class: "icon-credit-card" },
+  { name: "users.svg", class: "icon-users" },
+  { name: "crop-1.svg", class: "icon-crop-1" },
+  { name: "video.svg", class: "icon-video" },
+  { name: "crop.svg", class: "icon-crop" },
+  { name: "volume off.svg", class: "icon-volume-off" },
+  { name: "cub.svg", class: "icon-cub" },
+  { name: "volume up.svg", class: "icon-volume-up" },
+  { name: "currency-dollar.svg", class: "icon-currency-dollar" },
+  { name: "volumn down.svg", class: "icon-volumn-down" },
+  { name: "currency-euro.svg", class: "icon-currency-euro" },
+  { name: "watch.svg", class: "icon-watch" },
+  { name: "currency-rupee.svg", class: "icon-currency-rupee" },
+  { name: "wifi.svg", class: "icon-wifi" },
+  { name: "database.svg", class: "icon-database" },
+  { name: "window.svg", class: "icon-window" },
+  { name: "dislike.svg", class: "icon-dislike" },
+];
+
+// Updated icon loading function
+// Updated icon loading function
+async function loadSVGIcons(type) {
+  const iconsContainer = document.getElementById("icons-container");
+
+  // Clear any existing icons
+  iconsContainer.innerHTML = "";
+
+  // Limit to 5 icons per row
+  const iconsPerRow = 5;
+
+  svgIcons.forEach(async (file, index) => {
+    try {
+      const response = await fetch(`./assets/${file.name}`);
+      if (response.ok) {
+        const svgContent = await response.text();
+
+        // Create a wrapper div for each icon
+        const iconWrapper = document.createElement("div");
+
+        // Apply classes for consistent sizing and layout
+        iconWrapper.classList.add(
+          "flex",
+          "flex-col",
+          "items-center",
+          "justify-center",
+          "py-8",
+          "border",
+          "border-gray-200",
+          "rounded-lg",
+          "hover:bg-gray-100",
+          "transition-colors"
+        );
+
+        // Create a container for the SVG to control its size
+        const svgContainer = document.createElement("div");
+        svgContainer.classList.add(
+          "w-10", // width: 224px
+          "h-10", // height: 224p
+          "text-blue-400",
+          "bg-blue-400"
+        );
+
+        // Insert SVG content
+        svgContainer.innerHTML = svgContent;
+
+        // Modify the SVG to ensure it fills the container
+        const svg = svgContainer.querySelector("svg");
+        if (svg) {
+          svg.setAttribute("width", "100%");
+          svg.setAttribute("height", "100%");
+
+          svg.setAttribute("viewBox", "0 0 24 24"); // Adjust viewBox as needed
+        }
+        if (type == "stroke") {
+          svg.setAttribute("stroke-width", "3");
+        } else if (type == "fill") {
+          svg.setAttribute("stroke-width", "3");
+          svg.setAttribute("fill", "#000");
+        }
+
+        // Create a span for the icon name
+        const iconName = document.createElement("span");
+        iconName.textContent = file.name;
+        iconName.classList.add(
+          "text-sm",
+          "text-center",
+          "text-gray-600",
+          "mt-2"
+        );
+
+        // Assemble the icon wrapper
+        iconWrapper.appendChild(svgContainer);
+        iconWrapper.appendChild(iconName);
+
+        // Add to container
+        iconsContainer.appendChild(iconWrapper);
+      } else {
+        console.error(`Error loading ${file.name}: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error(`Failed to fetch ${file.name}:`, error);
+    }
+  });
+}
+
+loadSVGIcons("fill");
